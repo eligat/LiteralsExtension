6298e20931c8ab609894d7a3f464b02754cd27e0
Oleg Sannikov <olegsannikov@icerockdev.com>
2018-10-22 21:57:32 +0700

    Fix forced .png file extension

--- LiteralsExtension/ImageLiteralsConverter.swift
+++ LiteralsExtension/ImageLiteralsConverter.swift
@@ -9,11 +9,7 @@ struct ImageLiteralsConverter: TextConverter {
                                        range: range)
 
       for match in matches.reversed() {
-         var name = result.substring(with: match.rangeAt(1))
-         let pathExtension = (name as NSString).pathExtension
-         if pathExtension.isEmpty {
-            name = name + ".png"
-         }
+         let name = result.substring(with: match.rangeAt(1))
          result = result.replacingCharacters(in: match.range, with: "#imageLiteral(resourceName: \"\(name)\")") as NSString
       }
       return result as String
